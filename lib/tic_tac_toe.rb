19a65e01504f2e0915a4c46416ba7f3b27028ed4
Christopher Diep <diep.christopher@gmail.com>
2017-11-11 18:33:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,3 +42,15 @@ def valid_move?(board,index)
     return true
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(board,index)
+    move(board, index, char="X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
