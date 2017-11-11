6e1b08b21a057b493f81c814a422707614cac972
Christopher Diep <diep.christopher@gmail.com>
2017-11-11 18:39:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,3 +68,24 @@ end
 def current_player(board)
   turn_count(board).even? ? "X" : "O"
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
+  end
+  if !board.include?("X") || !board.include?("O")
+    return false
+  else
+    return false
+  end
+end
