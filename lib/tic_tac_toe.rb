d705d3b0177d002048de259a46fa1a882a106ff4
Christopher Diep <diep.christopher@gmail.com>
2017-11-11 18:23:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,10 +12,9 @@ WIN_COMBINATIONS = [
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  horizontal_space = "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts horizontal_space
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts horizontal_space
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
